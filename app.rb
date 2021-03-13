a44c52ba9d9f032da79a634e025735db8d70c72f
hiroforgithub <herohiro505@gmail.com>
2021-03-13 23:39:28 +0900

    ActiveRecord導入

--- app.rb
+++ app.rb
@@ -1,8 +1,18 @@
 require "sinatra"
 require "sinatra/reloader"
+require "active_record"
+
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "./bbs.db"
+)
+
+class Comment < ActiveRecord::Base
+end
 
 get "/" do
   @title = "BBS APP"
+  @comments = Comment.all
   erb :index
 end
 
